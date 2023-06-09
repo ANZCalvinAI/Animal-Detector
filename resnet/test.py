4ad2ce1f6b11b51f3786720621422193bc9cd6b7
CalvinAI <132722753+ANZCalvinAI@users.noreply.github.com>
2023-06-09 10:53:02 +1000

    Update test.py

--- resnet/test.py
+++ resnet/test.py
@@ -1,5 +1,6 @@
-import
-from torch import no_grad
+from os import listdir
+import numpy as np
+from torch import device, cuda, no_grad
 from torch.nn.functional import softmax
 from PIL import Image
 from utils import data_transform, get_weight_latest
@@ -14,6 +15,7 @@ path_project = "C:/Users/cz199/PycharmProjects/Animal-Detector/"
 # specify path parameters
 # =======================
 path_image = path_project + "datasets/iNat2021/images/"
+path_image_test = path_image + "test/"
 path_weight = path_project + "resnet/weights/"
 
 # =============
@@ -30,37 +32,42 @@ print(f"device\n{device}\n")
 set up the model as ResNet 152.
 use the most recently created weight in the weight path.
 """
-else:
-    # load the ResNet 152 model from local
-    model = resnet152(weights=None)
-    # search for the latest weight from local
-    weight_latest = get_weight_latest(path_weight)
-    # let state_dict load the latest weight from local
-    state_dict = load(path_weight + weight_latest)
-    # let the ResNet 152 model load the state_dict
-    model.load_state_dict(state_dict)
+# load the ResNet 152 model from local
+model = resnet152(weights=None)
+# search for the latest weight from local
+weight_latest = get_weight_latest(path_weight)
+# let state_dict load the latest weight from local
+state_dict = load(path_weight + weight_latest)
+# let the ResNet 152 model load the state_dict
+model.load_state_dict(state_dict)
 
-# =================
-# testing one image
-# =================
-input_image = Image.open(filename)
-preprocess = data_transform["test"]
+# =======
+# testing
+# =======
+probs_list = np.array([])
 
-input_tensor = preprocess(input_image)
-# create a mini batch as expected by the model
-input_batch = input_tensor.unsqueeze(0)
+for filename in listdir(path_image_test):
+    input_image = Image.open(path_image_test + filename)
+    preprocess = data_transform["test"]
+    input_tensor = preprocess(input_image)
+    # create a mini batch as expected by the model
+    input_batch = input_tensor.unsqueeze(0)
 
-with no_grad():
-    output = model(input_batch)
+    with no_grad():
+        output = model(input_batch)
     
-probabilities = softmax(output[0], dim=0)
+    probs = softmax(output[0], dim=0)
+    probs_list = np.append(
+        probs_list,
+        np.array([probs])
+    )
 
 # =====================
 # calculate top 5 error
 # =====================
-with open("imagenet_classes.txt", "r") as f:
-    categories = [s.strip() for s in f.readlines()]
-# Show top categories per image
-top5_prob, top5_catid = torch.topk(probabilities, 5)
-for i in range(top5_prob.size(0)):
-    print(categories[top5_catid[i]], top5_prob[i].item())
+# with open("imagenet_classes.txt", "r") as f:
+#     categories = [s.strip() for s in f.readlines()]
+# # Show top categories per image
+# top5_prob, top5_catid = torch.topk(probabilities, 5)
+# for i in range(top5_prob.size(0)):
+#     print(categories[top5_catid[i]], top5_prob[i].item())
