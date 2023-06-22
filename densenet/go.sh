fa1560a37ddbfcda5bac6cd557c09df50f16a657
Jeffrey <Jeffrey@example.com>
2023-06-22 10:37:31 +0000

    Update run.sh, go.sh

--- densenet/go.sh
+++ densenet/go.sh
@@ -1,9 +1,12 @@
-# transfer learning.
+# Fine tune.
+# tensorboard --bind_all --logdir=runs
+
 python -u train.py \
-	--data-path /home/ubuntu/dpinsw/classification-resnet/Animal-Detector/datasets/demo3-527classes/images/	\
-	--num_classes 527	\
-	--weights ./pre-weights/model-9.pth	\
-	--batch-size 555	\
-	--lr 0.05	\
-	--lrf 0.01	\
- 	--epochs 10
+        --data-path /home/ubuntu/dpinsw/classification-resnet/Animal-Detector/datasets/demo3-527classes/images/ \
+        --num_classes 527       \
+        --weights ./pre-weights/model-9.pth     \
+        --batch-size 200 \
+        --lr 0.1       \
+        --lrf 0.001      \
+        --epochs 8
+
--- densenet/run.sh
+++ densenet/run.sh
@@ -2,8 +2,8 @@
 nohup python -u train.py \
 	--data-path /home/ubuntu/dpinsw/classification-resnet/Animal-Detector/datasets/demo3-527classes/images/	\
 	--num_classes 527	\
-	--weights /home/ubuntu/dpinsw/classification-resnet/deep-learning-for-image-processing/pytorch_classification/Test8_densenet/pretrained/densenet161-8d451a50.pth	\
-	--batch-size 80	\
+	--weights ./pre-weights/model-9.pth	\
+	--batch-size 200	\
 	--lr 0.05	\
 	--lrf 0.01	\
  	--epochs 10	\
