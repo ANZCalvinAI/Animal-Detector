381647d27ffaefbfd8e9a536aceff585c2fd087b
Jeffrey H <jeffrey@example.com>
2023-06-23 10:41:57 +1000

    Densenet, done, but not the satisfying one.

--- densenet/go.sh
+++
@@ -1,9 +0,0 @@
-# transfer learning.
-python -u train.py \
-	--data-path /home/ubuntu/dpinsw/classification-resnet/Animal-Detector/datasets/demo3-527classes/images/	\
-	--num_classes 527	\
-	--weights ./pre-weights/model-9.pth	\
-	--batch-size 555	\
-	--lr 0.05	\
-	--lrf 0.01	\
- 	--epochs 10
