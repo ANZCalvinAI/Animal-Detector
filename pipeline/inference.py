0f1d0996bf31bce0de74c8dcb51121155eb22451
CalvinAI <132722753+ANZCalvinAI@users.noreply.github.com>
2023-06-24 14:09:58 +1000

    Update inference.py

--- pipeline/inference.py
+++ pipeline/inference.py
@@ -17,8 +17,8 @@ def inference(path_weight_yolo, path_images):
     yolov5 = torch.hub.load("ultralytics/yolov5", "custom", path=path_weight_yolo)
     images = list_files(path_images)
     out_detect = yolov5(images)
-    with open("output.txt", "w") as f:
+    with open("output.txt", "w") as file:
         for i, image in enumerate(images):
             out = out_detect.xyxy[i]
             for *xyxy, conf, cls in out:
-                f.write(f'{i} {out_detect.names[int(cls)]} {conf} {xyxy[0]} {xyxy[1]} {xyxy[2]} {xyxy[3]}\n')
+                file.write(f"{i} {out_detect.names[int(cls)]} {conf} {xyxy[0]} {xyxy[1]} {xyxy[2]} {xyxy[3]}\n")
