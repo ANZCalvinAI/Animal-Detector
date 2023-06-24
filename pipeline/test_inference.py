6832bfbd2d1bd5b40170e372639bc288c375b8bd
Ubuntu <ubuntu@ip-10-0-0-77.ap-southeast-2.compute.internal>
2023-06-24 11:25:27 +0000

    Test ./Animal-Detector/pipeline, done.

--- pipeline/test_inference.py
+++ pipeline/test_inference.py
@@ -15,4 +15,8 @@ path_images = path_project + "/datasets/pipeline/images"
 # =========
 # inference
 # =========
+
+path_weight_yolo = "weights_fine_tuned/yolov5x.pt"
+path_images = "./dataset"
+
 inference(path_weight_yolo=path_weight_yolo, path_images=path_images)
