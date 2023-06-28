d2bec2d4613524fa19edcc67f554b4737b262605
CalvinAI <132722753+ANZCalvinAI@users.noreply.github.com>
2023-06-28 17:09:18 +1000

    Update inference_full.py

--- pipeline/inference_full.py
+++ pipeline/inference_full.py
@@ -16,34 +16,40 @@ def list_files(direct):
 # ==============
 def detect(model_detector="yolov5", weight_detector, cls_custom, ci_custom=0.5, image):
     if model_detector != "yolov5":
-        raise ValueError("only 'yolov5' is supported")
+        raise ValueError("only 'yolov5' is supported for detector.")
     detector = torch.hub.load("ultralytics/" + model_detector, "custom", path=weight_detector)
     out = detector(image)
     out = out.pandas().xyxy[0]
     out = out[out["name"] == cls_custom]
+    
+    # case 1: nothing detected
     if out.empty:
         raise ValueError(f"no detected {cls_custom} with confidence interval {ci_custom}.")
     else:
+        # case 2: 1 detected
         if len(out) = 1:
-            print(f"only 1 detected {cls_custom} with confidence interval {ci_custom}")
+            print(f"only 1 detected {cls_custom} with confidence interval {ci_custom}.")
+        # case 3: 2 or more detected. select the 1 with highest confidence interval
         else:
             out = out.loc[out["confidence"].idxmax()]
             print(
                 f"2 or more detected {cls_custom} with condifence interval {ci_custom}.\n
                 select the 1 {cls_custom} with the highest confidence interval."
             )
+        # get the bounding box and cut the image based on the acquired bounding box
         xmin, ymin, xmax, ymax = out["xmin"], out["ymin"], out["xmax"], out["ymax"]
         image = cv2.imread(image)
         image_cropped = image[int(ymin):int(ymax), int(xmin):int(xmax)]
-    return image_cropped
+    return cls_custom, image_cropped
 
 
 # =====================
 # effnet classification
 # =====================
-def classify(model_classifier="effnet", weight_classifier, image_cropped):
+def classify(model_classifier="effnet", weight_classifier, out_detect):
     if model_classifier != "effnet":
-        raise ValueError("only 'effnet' is supported")
+        raise ValueError("only 'effnet' is supported for classifier.")
     classifier = effnet(weight_detector)
+    
 
 # ...
