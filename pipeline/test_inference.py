a02cac1f52d72537e0f308ed66b23f6eef22c3b7
CalvinAI <132722753+ANZCalvinAI@users.noreply.github.com>
2023-07-06 09:58:25 +1000

    Update test_inference.py

--- pipeline/test_inference.py
+++ pipeline/test_inference.py
@@ -1,10 +1,16 @@
 import os
 from inference import inference
 
-# configure path
+# configure paths
 path_pipeline = os.path.abspath(".")
+"""
 path_weight = os.path.join(path_pipeline, "weights")
 weight_detector = os.path.join(path_weight, "yolov5x.pt")
+"""
+path_asset = os.path.join(path_pipeline, "assets")
+weight_detector = os.path.join(path_asset, "yolov5x.pt")
+weight_classifier = None  # os.path.join(path_asset, "effnetv2.pt/.pth")
+utils_classifier = None
 
 path_project = os.path.abspath("..")
 path_dataset = os.path.join(path_project, "datasets\pipeline")
