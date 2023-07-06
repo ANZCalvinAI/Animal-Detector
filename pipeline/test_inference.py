d935d7807caee147ea1939a1610aeac4c07caba9
CalvinAI <132722753+ANZCalvinAI@users.noreply.github.com>
2023-07-06 21:54:18 +1000

    Update test_inference.py

--- pipeline/test_inference.py
+++ pipeline/test_inference.py
@@ -1,21 +1,21 @@
 import os
-from inference import inference
+from inference import batch_infer
 
-# configure paths
+# configure path
 path_pipeline = os.path.abspath(".")
-"""
 path_weight = os.path.join(path_pipeline, "weights")
 weight_detector = os.path.join(path_weight, "yolov5x.pt")
-"""
-path_asset = os.path.join(path_pipeline, "assets")
-weight_detector = os.path.join(path_asset, "yolov5x.pt")
-weight_classifier = None  # os.path.join(path_asset, "effnetv2.pt/.pth")
-utils_classifier = None
+weight_classifier = os.path.join(path_weight, "effnetv2m.pth")
 
 path_project = os.path.abspath("..")
 path_dataset = os.path.join(path_project, "datasets\pipeline")
 
 # load image
 image1 = os.path.join(path_dataset, "image1.jpg")
+image2 = os.path.join(path_dataset, "image2.jpeg")
 
-inference(image=image1, weight_detector=weight_detector, cls_custom="Insecta")
+output = batch_infer(
+    images=[image1, image2], cls_custom="Insecta",
+    weight_detector=weight_detector, weight_classifier=weight_classifier
+)
+print(output)
