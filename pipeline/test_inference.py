035a70e563b4872a4f836ab73a4ba5ac37cfcb03
CalvinAI <132722753+ANZCalvinAI@users.noreply.github.com>
2023-07-13 09:55:41 +1000

    Update test_inference.py

--- pipeline/test_inference.py
+++ pipeline/test_inference.py
@@ -1,18 +1,21 @@
 import os
-from inference import inference
+from inference import infer
 
-# ============
-# config paths
-# ============
-path_project = os.path.abspath("..")
+# configure paths
 path_pipeline = os.path.abspath(".")
+path_weight = os.path.join(path_pipeline, "weights")
+weight_detector = os.path.join(path_weight, "yolov5x.pt")
+weight_classifier = os.path.join(path_weight, "effnetv2m.pth")
 
-path_weight = path_pipeline + "/weights_fine_tuned"
-path_weight_yolo = path_weight + "/yolov5x.pt"
+path_project = os.path.abspath("..")
+path_dataset = os.path.join(path_project, "datasets\pipeline")
 
-path_images = path_project + "/datasets/pipeline/images"
+# load image
+image = os.path.join(path_dataset, "image1.jpg")
 
-# =========
-# inference
-# =========
-inference(path_weight_yolo=path_weight_yolo, path_images=path_images)
+output = infer(
+    image=image, cls_custom="Insecta",
+    weight_detector=weight_detector, weight_classifier=weight_classifier
+)
+output = [cls[0] for cls in output[0]]
+print(f"output\n{output}")
